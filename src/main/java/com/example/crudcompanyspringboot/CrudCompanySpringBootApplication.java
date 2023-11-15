251e57dd4965a1242917e609b73a1426da648a36
Nicolas Colque <nicolascon93@gmail.com>
2023-11-15 13:39:25 -0400

    first commit

---
+++ src/main/java/com/example/crudcompanyspringboot/CrudCompanySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudcompanyspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudCompanySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudCompanySpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crudcompanyspringboot/CrudCompanySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudcompanyspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudCompanySpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
